ce603d1ec2eb3cd03ca21276e0eff4b510941d9e
trabajos de Nicolas Fernandez <nicolas.fernandez.am@gmail.com>
2023-05-31 19:20:39 -0300

    Practico0 Finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,15 @@
+let base1 = 5
+
+let altura1 = 3 
+
+console.log(base1 * altura1)
+
+// ¿Cuál es el área de un rectángulo de base 5cm y altura 3cm? resultado = 15
+
+let base2 = 12.5 
+
+let altura2 = 5.3
+
+console.log(base2 * altura2)
+
+// ¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm? resultado = 66.25 
