01d844ac8fb2b2be64f944b6a00fd32372958152
Nicolas <122710707+TrabajosdeNicolasFernandez@users.noreply.github.com>
2023-06-14 22:51:41 -0300

    Delete practico1 directory

--- practico1/miNombreCompleto.js
+++
@@ -1,7 +0,0 @@
-let nombre = "Nicolas";
-
-let apellido = "Fernandez";
-
-let edad = 31;
-
-console.log("Hola, " + nombre + " " + apellido + ". Tienes " + edad + " años.");
--- practico1/miNombreObjecto.js
+++
@@ -1,3 +0,0 @@
-let persona = {nombre: "Nicolas", apellido: "Fernandez", edad: 31};
-
-console.log("Mi objeto persona es el siguiente: " + JSON.stringify(persona));
